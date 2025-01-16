382f10e605339eb2eee30f4de214a07cf8a92fe9
yunahui <yunahui@naver.com>
2025-01-16 09:16:03 +0900

    default setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,12 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    // Custom Rules
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto', // Windows CRLF
+      },
+    ],
   },
 };
